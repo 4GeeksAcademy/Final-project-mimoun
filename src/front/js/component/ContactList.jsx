3e9cc8b3741d049da12e0a7250370c07dbe2cf4f
Mimoun El Yazidi <87827134+min45@users.noreply.github.com>
2025-01-25 09:58:31 +0000

    styling according to pep8

--- src/front/js/component/ContactList.jsx
+++ src/front/js/component/ContactList.jsx
@@ -1,47 +1,26 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { Context } from "../store/appContext.js";
+
 
 export const ContactList = () => {
   const { store, actions } = useContext(Context);
-  const baseURL = "https://playground.4geeks.com/contact";
-  const user = "mimoun";
-
-  
-  const getContacts = async () => {
-    const uri = `${baseURL}/agendas/${user}`;
-    const options = {
-      method: "GET",
-    };
-
-    const response = await fetch(uri, options);
-
-    if (!response.ok) {
-      console.error("Error:", response.status, response.statusText);
-      return;
-    }
-
-    const data = await response.json(); 
-    actions.setContacts(data); 
-  };
-
  
-  useEffect(() => {
-    getContacts();
-  }, []);
-
   return (
     <div className="container">
-      <ul>
+      <ul className="text-center">
         {store.contacts && store.contacts.length > 0 ? (
-          store.contacts.map((contact, index) => (
-            <li key={index}>
+          store.contacts.map((contact) => (
+            <li key={contact.id}>
               <strong>{contact.name}</strong>
               <br />
               {contact.email}
               <br />
               {contact.phone}
               <br />
-              {contact.adress}
+              {contact.address}
+              <span onClick={actions.addContact}> <i  class="fa-solid fa-address-book"></i>  </span>
+              <span onClick={() => actions.deleteContact(contact.id)}><i  class="fa-solid fa-trash"></i></span>
+              
             </li>
           ))
         ) : (
